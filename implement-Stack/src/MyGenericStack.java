5d6f7272d2da2f81cb07831f70ec6437636d6f68
unknown <ngocduc.2109@gmail.com>
2021-04-16 11:56:55 +0700

    [Thực hành] Triển khai stack sử dụng lớp LinkedList trong thư viện java.util

---
+++ implement-Stack/src/MyGenericStack.java
@@ -0,0 +1,32 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack(){
+        stack = new LinkedList<>();
+    }
+
+    public int size(){
+        return stack.size();
+    }
+
+    public boolean isEmpty(){
+        if (size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public void push(T element){
+        stack.addFirst(element);
+    }
+
+    public T pop(){
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+}
